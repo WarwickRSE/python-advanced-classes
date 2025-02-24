fdf1ed474195e9c2848e47b95bf05bc487c93ed2
PipGrylls <pipgryllsastro@gmail.com>
2025-02-24 11:11:34 +0000

    typos and figures

--- helpers.py
+++ helpers.py
@@ -16,7 +16,14 @@ class SuperBase(object):
         print('Exit SuperBase.__init__')
 
 
-__all__ = [Base, SuperBase]
+class MIBase(object):
+    def __init__(self, *args, **kwargs):
+        print('Enter MIBase.__init__')
+        super().__init__(*args, **kwargs)
+        print('Exit MIBase.__init__')
+
+
+__all__ = [Base, SuperBase, MIBase]
 
 if __name__ == '__main__':
     print('This script is not meant to be run directly.')
